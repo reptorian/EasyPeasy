04e35c503c5156638dbc0c8fcafeaca4e7284585
Grzegorz Wierzejski <wierzejski.grzegorz@gmail.com>
2017-01-09 11:48:00 +0100

    dodano menu.html i menucontroller.java, edytowano home i admin

---
+++ spring-security-mvc-login/src/main/java/com/baeldung/thymeleaf/controller/MenuController.java
@@ -0,0 +1,17 @@
+package com.baeldung.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MenuController {
+	
+	@RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String getHome(Model model) {
+
+        return "menu.html";
+    }
+
+}
